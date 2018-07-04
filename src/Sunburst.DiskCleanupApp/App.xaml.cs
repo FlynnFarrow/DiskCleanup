d7e277f4f6d1c0e080310b808d5be935bcd764d1
William Kent <wjk@users.noreply.github.com>
2018-07-04 12:47:11 -0400

    Remove pointless comment

--- src/Sunburst.DiskCleanupApp/App.xaml.cs
+++ src/Sunburst.DiskCleanupApp/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace Sunburst.DiskCleanupApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
 
