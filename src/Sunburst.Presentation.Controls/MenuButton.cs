5f656a93380d329ef21325c773acdb096bd4951b
William Kent <wjk@users.noreply.github.com>
2018-07-04 14:41:46 -0400

    Add MenuButton class and its default theme
    This control is fully implemented and working, save for a
    single bug regarding mouse-over/mouse-out handling that I
    have not yet ratted out.

---
+++ src/Sunburst.Presentation.Controls/MenuButton.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace Sunburst.Presentation.Controls
+{
+    [TemplateVisualState(Name = "Enabled", GroupName = "EnabledStates")]
+    [TemplateVisualState(Name = "Disabled", GroupName = "EnabledStates")]
+    [TemplateVisualState(Name = "MenuEmpty", GroupName = "EnabledStates")]
+    [TemplateVisualState(Name = "Normal", GroupName = "FocusStates")]
+    [TemplateVisualState(Name = "Focused", GroupName = "FocusStates")]
+    [TemplateVisualState(Name = "MouseOver", GroupName = "FocusStates")]
+    [TemplateVisualState(Name = "Pressed", GroupName = "FocusStates")]
+    [ContentProperty("Menu")]
+    public class MenuButton : ContentControl
+    {
+        public static readonly DependencyProperty MenuProperty =
+            DependencyProperty.Register(nameof(Menu), typeof(ContextMenu), typeof(MenuButton), new PropertyMetadata(null, OnMenuChanged));
+
+        public ContextMenu Menu
+        {
+            get => (ContextMenu)GetValue(MenuProperty);
+            set => SetValue(MenuProperty, value);
+        }
+
+        private static void OnMenuChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            MenuButton button = (MenuButton)sender;
+            button.UpdateStates(true);
+
+            if (e.OldValue != null)
+                ((ContextMenu)e.OldValue).Closed -= button.OnMenuClosed;
+
+            if (e.NewValue != null)
+            {
+                ContextMenu menu = (ContextMenu)e.NewValue;
+                menu.PlacementTarget = button;
+                menu.Placement = System.Windows.Controls.Primitives.PlacementMode.Bottom;
+                menu.Closed += button.OnMenuClosed;
+            }
+        }
+
+        private void OnMenuClosed(object sender, RoutedEventArgs e)
+        {
+            UpdateStates(true);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            UpdateStates(false);
+        }
+
+        private void UpdateStates(bool useTransitions)
+        {
+            if (IsEnabled) VisualStateManager.GoToState(this, "Enabled", useTransitions);
+
+            if (Menu.IsOpen) VisualStateManager.GoToState(this, "Pressed", useTransitions);
+            else if (IsMouseOver) VisualStateManager.GoToState(this, "MouseOver", useTransitions);
+            else if (IsFocused) VisualStateManager.GoToState(this, "Focused", useTransitions);
+            else VisualStateManager.GoToElementState(this, "Normal", useTransitions);
+
+            if (!IsEnabled) VisualStateManager.GoToElementState(this, "Disabled", useTransitions);
+        }
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            UpdateStates(true);
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            UpdateStates(true);
+        }
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+            UpdateStates(true);
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e)
+        {
+            base.OnMouseLeave(e);
+            UpdateStates(true);
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            Focus();
+            if (IsEnabled) ToggleMenu();
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            if (IsFocused)
+            {
+                if (e.Key == Key.Space || e.Key == Key.Enter)
+                {
+                    ToggleMenu();
+                }
+            }
+        }
+
+        private void ToggleMenu()
+        {
+            ContextMenu menu = Menu;
+            if (menu == null || menu.Items.Count == 0) return;
+            menu.IsOpen = !menu.IsOpen;
+        }
+    }
+}
